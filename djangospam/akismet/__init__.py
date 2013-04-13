eb2f0dfb1b5cfbb74343a8ef89ec1a4168757dd3
Leandro Arndt <poetalirico@gmail.com>
2013-04-12 21:46:39 -0300

    DeprecationWarning at djangospam.akismet

--- djangospam/akismet/__init__.py
+++ djangospam/akismet/__init__.py
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from moderator import AlreadyModerated, register, Akismet, AkismetError
+import moderator
+import warnings
+
+def register(*args, **kwargs):
+    warnings.warn("Using 'register' from djangospam.akismet is deprecated. \
+Import it from djangospam.akismet.moderator instead.",\
+    category=DeprecationWarning, stacklevel=2)
+    return register.moderator(*args, **kwargs)
+
+class AlreadyModerated(moderator.AlreadyModerated):
+    def __init__(self, *args, **kwargs):
+        warnings.warn("Using 'AlreadyModerated' from djangospam.akismet is \
+deprecated. Import it from djangospam.akismet.moderator instead."
+        , category=DeprecationWarning, stacklevel=2)
+        super(AlreadyModerated, self).__init__(*args, **kwargs)
+
+class Akismet(moderator.AlreadyModerated):
+    def __init__(self, *args, **kwargs):
+        warnings.warn("Using 'Akismet' from djangospam.akismet is \
+deprecated. Import it from djangospam.akismet.moderator instead."
+        , category=DeprecationWarning, stacklevel=2)
+        super(Akismet, self).__init__(*args, **kwargs)
+
+class AkismetError(moderator.AlreadyModerated):
+    def __init__(self, *args, **kwargs):
+        warnings.warn("Using 'AkismetError' from djangospam.akismet is \
+deprecated. Import it from djangospam.akismet.moderator instead."
+        , category=DeprecationWarning, stacklevel=2)
+        super(AkismetError, self).__init__(*args, **kwargs)
